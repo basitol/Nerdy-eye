9651b8867d274a6fb1b2210de46982fc7d442490
basitol <41623944+basitol@users.noreply.github.com>
2022-05-10 22:59:01 +0100

    Added router and change theme function with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { Navbar } from "./components";
+import { BrowserRouter } from "react-router-dom";
+import { useState } from "react";
 
 export default function App() {
+  const [theme, setTheme] = useState(false);
+
+  function handleToggle() {
+    setTheme((prevState) => !prevState);
+    console.log("clicked");
+  }
+
   return (
-    <div className="">
-      <Navbar />
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="">
+        <Navbar darkmode={theme} toggledarkmode={handleToggle} />
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </BrowserRouter>
   );
 }
