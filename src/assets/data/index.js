7f158cffd47e549f8b50dc262a37ac770115151f
basitol <41623944+basitol@users.noreply.github.com>
2022-05-10 23:00:23 +0100

    Create menu Data

---
+++ src/assets/data/index.js
@@ -0,0 +1,38 @@
+export const menuData = [
+  {
+    id: 1,
+    title: "Services",
+    // link: dashboardPage,
+    icon: ["helpee", "helper"],
+  },
+  {
+    id: 2,
+    title: "Academy",
+    // link: transactionsPage,
+    icon: ["helpee"],
+  },
+  {
+    id: 3,
+    title: "Community",
+    // link: investmentPage,
+    user: ["helpee"],
+  },
+  {
+    id: 4,
+    title: "Workforce",
+    // link: ourPlansPage,
+    user: ["helpee"],
+  },
+  {
+    id: 5,
+    title: "Store",
+    // link: profilePage,
+    user: ["helpee"],
+  },
+  {
+    id: 6,
+    title: "Affiliate",
+    // link: referralsPage,
+    user: ["helpee"],
+  },
+];
