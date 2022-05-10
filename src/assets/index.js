24dffd14825d6bc93516ea5a502d86c0444fad65
basitol <41623944+basitol@users.noreply.github.com>
2022-05-10 22:59:47 +0100

    Modified export

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,16 @@
 import logo from "./logo.png";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart, FiChevronDown } from "react-icons/fi";
+import { BiUser } from "react-icons/bi";
+import leftLightStroke from "./Vector-0-l.png";
+import rightLightStroke from "./Vector-t-l.png";
 
-export { logo };
+export {
+  logo,
+  AiOutlineMenu,
+  FiShoppingCart,
+  BiUser,
+  FiChevronDown,
+  leftLightStroke,
+  rightLightStroke,
+};
