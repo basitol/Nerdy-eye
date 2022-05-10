cadc91737ba933be487593ac45bd188401493ec4
basitol <41623944+basitol@users.noreply.github.com>
2022-05-10 23:03:56 +0100

    Build export for reusables

---
+++ src/components/reusables/index.js
@@ -0,0 +1,5 @@
+import IconContainer from "./IconContainer";
+import CustomLink from "./Links/CustomLink";
+import CustomNavLink from "./Links/CustomNavLink";
+
+export { IconContainer, CustomLink, CustomNavLink };
