3212d0643dd4cd93dadd854348ff7ad75648d9e2
basitol <41623944+basitol@users.noreply.github.com>
2022-05-11 02:23:35 +0100

    Added export for dark logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,16 +1,21 @@
 import logo from "./logo.png";
+import logoDark from "./logo2.png";
+
 import { AiOutlineMenu } from "react-icons/ai";
 import { FiShoppingCart, FiChevronDown } from "react-icons/fi";
 import { BiUser } from "react-icons/bi";
+import { IoMdClose } from "react-icons/io";
 import leftLightStroke from "./Vector-0-l.png";
 import rightLightStroke from "./Vector-t-l.png";
 
 export {
   logo,
+  logoDark,
   AiOutlineMenu,
   FiShoppingCart,
   BiUser,
   FiChevronDown,
+  IoMdClose,
   leftLightStroke,
   rightLightStroke,
 };
