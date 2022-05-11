494c09917ce7afa3a712f69e0f189097334dbd34
basitol <41623944+basitol@users.noreply.github.com>
2022-05-11 17:22:05 +0100

    modified exports

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 import logo from "./logo.png";
 import logoDark from "./logo2.png";
+import profile from "./profile-card.png";
+import profileLg from "./profile-card-lg.png";
 
 import { AiOutlineMenu } from "react-icons/ai";
 import { FiShoppingCart, FiChevronDown } from "react-icons/fi";
@@ -7,10 +9,14 @@ import { BiUser } from "react-icons/bi";
 import { IoMdClose } from "react-icons/io";
 import leftLightStroke from "./Vector-0-l.png";
 import rightLightStroke from "./Vector-t-l.png";
+import leftDarkStroke from "./Vector-one-dark.png";
+import rightDarkStroke from "./Vector-two-dark.png";
 
 export {
   logo,
   logoDark,
+  profile,
+  profileLg,
   AiOutlineMenu,
   FiShoppingCart,
   BiUser,
@@ -18,4 +24,6 @@ export {
   IoMdClose,
   leftLightStroke,
   rightLightStroke,
+  leftDarkStroke,
+  rightDarkStroke,
 };
