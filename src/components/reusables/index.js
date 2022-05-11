cdd3598f3cdb3c66b161921592bd98bf343c3047
basitol <41623944+basitol@users.noreply.github.com>
2022-05-11 02:28:42 +0100

    Added custom button to export

--- src/components/reusables/index.js
+++ src/components/reusables/index.js
@@ -1,5 +1,6 @@
 import IconContainer from "./IconContainer";
 import CustomLink from "./Links/CustomLink";
 import CustomNavLink from "./Links/CustomNavLink";
+import CustomButton from "./CustomButton";
 
-export { IconContainer, CustomLink, CustomNavLink };
+export { IconContainer, CustomLink, CustomNavLink, CustomButton };
