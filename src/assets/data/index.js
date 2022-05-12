694f7a881a024b5786f93146d878d63820c14968
basitol <41623944+basitol@users.noreply.github.com>
2022-05-12 23:33:52 +0100

    Created data for components

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,3 +1,13 @@
+import {
+  reactImg,
+  phoneImg,
+  graphicsImg,
+  developerImg,
+  meta,
+  appleImg,
+  amazonImg,
+} from "../../assets";
+
 export const menuData = [
   {
     id: 1,
@@ -34,5 +44,90 @@ export const menuData = [
     title: "Affiliate",
     // link: referralsPage,
     user: ["helpee"],
+  },
+];
+
+export const careerData = [
+  {
+    id: 1,
+    title: "UI/UX DESIGN",
+    available: "120 Jobs Available",
+    image: phoneImg,
+  },
+  {
+    id: 2,
+    title: "React Developer",
+    available: "120 Jobs Available",
+    image: reactImg,
+  },
+  {
+    id: 3,
+    title: "Graphics Design",
+    available: "120 Jobs Available",
+    image: graphicsImg,
+    user: ["helpee"],
+  },
+  {
+    id: 4,
+    title: "Developer",
+    available: "120 Jobs Available",
+    image: developerImg,
+  },
+];
+
+export const jobData = [
+  {
+    id: 1,
+    job: "Design",
+    title: "UI Designer",
+    jobLocation: "Lagos",
+    type: "Full Time",
+    industry: "Meta",
+    pathImage: phoneImg,
+    image: meta,
+  },
+  {
+    id: 2,
+    job: "Web development",
+    title: "Web Developer",
+    jobLocation: "Lagos",
+    type: "Full Time",
+    industry: "Amazon",
+    pathImage: developerImg,
+    imgClass: "devImg",
+    image: amazonImg,
+  },
+  {
+    id: 3,
+    job: "Software Development",
+    title: "ReactJs Developer",
+    jobLocation: "Lagos",
+    type: "Full Time",
+    industry: "Apple",
+    pathImage: reactImg,
+    imgClass: "devImg",
+    image: appleImg,
+  },
+  {
+    id: 4,
+    job: "Graphics Design",
+    title: "Graphics Designer",
+    jobLocation: "Lagos",
+    type: "Full Time",
+    industry: "Meta",
+    pathImage: graphicsImg,
+    imgClass: "devImg",
+
+    image: meta,
+  },
+  {
+    id: 5,
+    job: "Design",
+    title: "UI Designer",
+    jobLocation: "Lagos",
+    type: "Full Time",
+    industry: "Meta",
+    pathImage: phoneImg,
+    image: meta,
   },
 ];
