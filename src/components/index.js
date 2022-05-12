14a546a021bbb629a2116d484ff642044a7e4881
basitol <41623944+basitol@users.noreply.github.com>
2022-05-12 23:34:23 +0100

    created export for components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
+import Works from "./Works/Works";
+import Toggle from "./Toggle/Toggle";
+import Job from "./Job/Job";
+import Jobs from "./Jobs/Jobs";
+import Career from "./Career/Career";
 
-export { Navbar, Hero };
+export { Navbar, Hero, Works, Toggle, Job, Jobs, Career };
