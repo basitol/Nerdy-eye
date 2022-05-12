231cf1d00da735a0677de2dd9c0c7747d00515bd
basitol <41623944+basitol@users.noreply.github.com>
2022-05-12 23:33:28 +0100

    Added export and import for new assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,6 +2,19 @@ import logo from "./logo.png";
 import logoDark from "./logo2.png";
 import profile from "./profile-card.png";
 import profileLg from "./profile-card-lg.png";
+import work from "./work.png";
+import workdark from "./workdark.png";
+import triangle from "./Polygon.png";
+import phoneImg from "./UI.png";
+import reactImg from "./react.png";
+import graphicsImg from "./graphics.png";
+import developerImg from "./developer.png";
+import signature from "./signature.png";
+import map from "./map.png";
+import meta from "./meta.png";
+import appleImg from "./apple.png";
+import amazonImg from "./amazon.png";
+import offer from "./offer.png";
 
 import { AiOutlineMenu } from "react-icons/ai";
 import { FiShoppingCart, FiChevronDown } from "react-icons/fi";
@@ -17,12 +30,25 @@ export {
   logoDark,
   profile,
   profileLg,
+  workdark,
+  triangle,
+  phoneImg,
+  reactImg,
+  graphicsImg,
+  developerImg,
+  signature,
+  map,
+  meta,
+  appleImg,
+  amazonImg,
+  offer,
   AiOutlineMenu,
   FiShoppingCart,
   BiUser,
   FiChevronDown,
   IoMdClose,
   leftLightStroke,
+  work,
   rightLightStroke,
   leftDarkStroke,
   rightDarkStroke,
