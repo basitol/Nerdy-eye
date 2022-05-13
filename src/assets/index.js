1a4a9dd7758ace905b136e5843ae017d3c9c4745
basitol <41623944+basitol@users.noreply.github.com>
2022-05-13 22:37:44 +0100

    Added export for new icons and assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -15,11 +15,28 @@ import meta from "./meta.png";
 import appleImg from "./apple.png";
 import amazonImg from "./amazon.png";
 import offer from "./offer.png";
+import transportation from "./transportation.png";
+import health from "./health.png";
+import commerce from "./commerce.png";
+import media from "./media.png";
+import fintech from "./fintech.png";
+import hospitality from "./hospitality.png";
+import hands from "./hands.png";
+import reason from "./reason.png";
 
 import { AiOutlineMenu } from "react-icons/ai";
-import { FiShoppingCart, FiChevronDown } from "react-icons/fi";
+import {
+  FiShoppingCart,
+  FiChevronDown,
+  FiTwitter,
+  FiLinkedin,
+} from "react-icons/fi";
 import { BiUser } from "react-icons/bi";
 import { IoMdClose } from "react-icons/io";
+import { BsCheck2 } from "react-icons/bs";
+import { MdLocationPin, MdOutlineMail, MdLocalPhone } from "react-icons/md";
+import { AiOutlineInstagram, AiOutlineYoutube } from "react-icons/ai";
+import { BsAsterisk } from "react-icons/bs";
 import leftLightStroke from "./Vector-0-l.png";
 import rightLightStroke from "./Vector-t-l.png";
 import leftDarkStroke from "./Vector-one-dark.png";
@@ -42,11 +59,28 @@ export {
   appleImg,
   amazonImg,
   offer,
+  transportation,
+  health,
+  commerce,
+  media,
+  fintech,
+  hospitality,
+  hands,
+  reason,
   AiOutlineMenu,
   FiShoppingCart,
   BiUser,
   FiChevronDown,
   IoMdClose,
+  MdLocationPin,
+  MdOutlineMail,
+  MdLocalPhone,
+  AiOutlineInstagram,
+  AiOutlineYoutube,
+  FiTwitter,
+  FiLinkedin,
+  BsCheck2,
+  BsAsterisk,
   leftLightStroke,
   work,
   rightLightStroke,
