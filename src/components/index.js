48ae82ea36e42e558b4a860c1534d8382023e13e
basitol <41623944+basitol@users.noreply.github.com>
2022-05-13 22:44:41 +0100

    Added export for components

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,26 @@ import Toggle from "./Toggle/Toggle";
 import Job from "./Job/Job";
 import Jobs from "./Jobs/Jobs";
 import Career from "./Career/Career";
+import Industries from "./Industries/Industries";
+import Industry from "./Industry/Industry";
+import Footer from "./Footer/Footer";
+import Stories from "./Stories/Stories";
+import Collaborate from "./Collaborate/Collaborate";
+import Reason from "./Reason/Reason";
+import customInput from "./CustomInput/CustomInput";
 
-export { Navbar, Hero, Works, Toggle, Job, Jobs, Career };
+export {
+  Navbar,
+  Hero,
+  Works,
+  Toggle,
+  Job,
+  Jobs,
+  Career,
+  Industries,
+  Industry,
+  Footer,
+  Stories,
+  Collaborate,
+  Reason,
+};
